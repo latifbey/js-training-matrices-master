c22cc16af5b38fcf32d620b87837464e4bb089d4
latifbey <latifbey@yandex.com>
2017-09-17 14:51:43 +0200

    komm schon

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+/**
+ * Please provide all your functions in this file.
+ * Consider using extending built-in objects.
+ */
+ 
+"use strict";
+
+function add(mx){}
+
+function substract(mx){}
+
+function multiply(mx){}
+
+function display(){}
