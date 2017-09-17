9076fd4bc60fe05bfddc14f101c9d3b458b2b77c
latifbey <latifbey@yandex.com>
2017-09-17 18:58:02 +0200

    ich fange an

--- js/functions.js
+++ js/functions.js
@@ -5,10 +5,38 @@
  
 "use strict";
 
-function add(mx){}
+function add(mx,my){
+    var result=[];
+    result=new Array(mx.length);
+    for(var i=0; i<result.length; i++){
+        result[i]=new Array(mx[i].length);
+        for(var j=0; j<result[i].length; j++){
+            result[i][j]=mx[i][j]+my[i][j];
+        }
+    }
+    return result;
+}
 
-function substract(mx){}
-
-function multiply(mx){}
-
+function substract(mx,my){
+    var result=[];
+    result=new Array(mx.length);
+    for(var i=0; i<result.length; i++){
+        result[i]=new Array(mx[i].length);
+        for(var j=0; j<result[i].length; j++){
+            result[i][j]=mx[i][j]-my[i][j];
+        }
+    }
+    return result;
+}
+function multiply(mx,my){
+    var result=[];
+    result=new Array(mx.length);
+    for(var i=0; i<result.length; i++){
+        result[i]=new Array(mx[i].length);
+        for(var j=0; j<result[i].length; j++){
+            result[i][j]=mx[i][j]*my[i][j];
+        }
+    }
+    return result;
+}
 function display(){}
